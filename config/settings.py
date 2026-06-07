713d5d826a6c5455dc64aaad9d8129d69c34d042
emma159-code <akandaemmanuella9@icloud.com>
2026-06-07 18:05:28 +0200

    Enable debug for CSRF troubleshooting

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hsf*-pub(5k&^%ba58lk0q_l0=*n#o+cl#nz3=v5t=^z=i_1m6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
